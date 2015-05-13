8ddbea3476299f2ecde8203e5327933c336c42a5
bfrench <bfrench@andrew.cmu.edu>
2015-05-13 13:42:51 -0400

    implemented dateentereddecorator to look up the given date in the database and color the cell background if there is an entry for it.
    moved the test code to the holiday decorator since I will probably be
    wanting to write text there if I get it working.

--- ShiffmanCalendar/src/com/example/shiffmancalendar/DateEnteredDecorator.java
+++ ShiffmanCalendar/src/com/example/shiffmancalendar/DateEnteredDecorator.java
@@ -3,6 +3,7 @@ package com.example.shiffmancalendar;
 import java.util.Calendar;
 import java.util.Date;
 
+import android.content.ContentValues;
 import android.graphics.Color;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -16,23 +17,14 @@ public class DateEnteredDecorator implements CalendarCellDecorator {
 
 	@Override
 	public void decorate(CalendarCellView cellView, Date date) {
-		String dateString = Integer.toString(date.getDate());
-		Calendar comp = Calendar.getInstance();
-		comp.clear();
-		comp.set(2015, 4, 15, 0, 0, 0);
-		if (date.getDate() == 15 && date.getMonth() == 4) {
-		System.out.println("DATE STRING: " + date.getTime());
-		System.out.println("DATE STRING: " + comp.getTime().getTime());
+		
+		DBHelper db = new DBHelper(cellView.getContext());
+		ContentValues value = db.entryExists(date.getTime());
+		
+		if (value != null) {
+			cellView.setBackgroundColor(Color.GREEN);
 		}
-		if (date.equals(comp.getTime())) {
-		    SpannableString string = new SpannableString(dateString + "\ndone");
-		    string.setSpan(new RelativeSizeSpan(0.5f), 0, dateString.length(),
-		        Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-		    cellView.setText(string);
-		    cellView.setBackgroundColor(Color.GREEN);
-		} else {
-			//System.out.println("DATE STRING: no match");
-		}
+	
 	}
 
 }
--- ShiffmanCalendar/src/com/example/shiffmancalendar/HolidayDecorator.java
+++ ShiffmanCalendar/src/com/example/shiffmancalendar/HolidayDecorator.java
@@ -1,6 +1,12 @@
 package com.example.shiffmancalendar;
 
+import java.util.Calendar;
 import java.util.Date;
+
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.RelativeSizeSpan;
 
 import com.squareup.timessquare.CalendarCellDecorator;
 import com.squareup.timessquare.CalendarCellView;
@@ -9,8 +15,23 @@ public class HolidayDecorator implements CalendarCellDecorator {
 
 	@Override
 	public void decorate(CalendarCellView cellView, Date date) {
-		// TODO Auto-generated method stub
-		
+		String dateString = Integer.toString(date.getDate());
+		Calendar comp = Calendar.getInstance();
+		comp.clear();
+		comp.set(2015, 4, 15, 0, 0, 0);
+		if (date.getDate() == 15 && date.getMonth() == 4) {
+		System.out.println("DATE STRING: " + date.getTime());
+		System.out.println("DATE STRING: " + comp.getTime().getTime());
+		}
+		if (date.equals(comp.getTime())) {
+		    SpannableString string = new SpannableString(dateString + "\ndone");
+		    string.setSpan(new RelativeSizeSpan(0.5f), 0, dateString.length(),
+		        Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+		    cellView.setText(string);
+		    cellView.setBackgroundColor(Color.GREEN);
+		} else {
+			//System.out.println("DATE STRING: no match");
+		}
 	}
 
 }
