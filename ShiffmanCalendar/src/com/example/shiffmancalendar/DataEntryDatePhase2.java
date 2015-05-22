8141206cdb8e3a67db7cd60a4c750ca9b86a84c8
bfrench <bfrench@andrew.cmu.edu>
2015-05-22 19:18:46 -0400

    loaded the appropriate content layouts for phase 2 and 3. still need to figure out exactly what needs to be in them

--- ShiffmanCalendar/src/com/example/shiffmancalendar/DataEntryDatePhase2.java
+++ ShiffmanCalendar/src/com/example/shiffmancalendar/DataEntryDatePhase2.java
@@ -1,7 +1,16 @@
 package com.example.shiffmancalendar;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class DataEntryDatePhase2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.data_phase2);
+	}
 
 }
--- ShiffmanCalendar/src/com/example/shiffmancalendar/DataEntryDatePhase3.java
+++ ShiffmanCalendar/src/com/example/shiffmancalendar/DataEntryDatePhase3.java
@@ -1,7 +1,15 @@
 package com.example.shiffmancalendar;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class DataEntryDatePhase3 extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.data_phase3);
+	}
 }
