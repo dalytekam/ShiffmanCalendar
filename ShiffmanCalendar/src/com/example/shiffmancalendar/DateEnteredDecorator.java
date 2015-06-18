5399213da3894117cd4a7ca465313e01e87ff11a
bfrench <bfrench@andrew.cmu.edu>
2015-06-18 17:08:43 -0400

    updated the date entered decorator to use the new entryExists signature

--- ShiffmanCalendar/src/com/example/shiffmancalendar/DateEnteredDecorator.java
+++ ShiffmanCalendar/src/com/example/shiffmancalendar/DateEnteredDecorator.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 import android.content.ContentValues;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -17,9 +18,12 @@ public class DateEnteredDecorator implements CalendarCellDecorator {
 
 	@Override
 	public void decorate(CalendarCellView cellView, Date date) {
+		SharedPreferences prefs = cellView.getContext().getSharedPreferences("shiffman_calendar", 0);
+		String id = prefs.getString("id", "unknown");
+		String session = prefs.getString("session", "unknown");
 		
 		DBHelper db = new DBHelper(cellView.getContext());
-		ContentValues value = db.entryExists(date.getTime());
+		ContentValues value = db.entryExists(id, session, date.getTime());
 		
 		if (value != null) {
 			cellView.setBackgroundColor(cellView.getResources().getColor(R.color.LightGreen));
