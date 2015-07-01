1aa6f894e6b9f0f5131dadb9f94709ccad7f99a3
bfrench <bfrench@andrew.cmu.edu>
2015-07-01 16:29:23 -0400

    added column filters for the summarize data activity so only relevant columns are displayed for each of the study variants.

--- ShiffmanCalendar/src/com/example/shiffmancalendar/SummarizeData.java
+++ ShiffmanCalendar/src/com/example/shiffmancalendar/SummarizeData.java
@@ -1,16 +1,15 @@
 package com.example.shiffmancalendar;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -29,17 +28,37 @@ public class SummarizeData extends Activity {
 	Button applyID;
 	Context context;
 	
+	public static final String[] COMMON_FILTER = { DBHelper.KEY_DATE_ENTERED, DBHelper.KEY_PID, DBHelper.KEY_STUDY,
+												   DBHelper.KEY_SESSION, DBHelper.KEY_DATE };
+	public static final String[][] STUDY_FILTER = { { DBHelper.KEY_CIG_COUNT },
+													{ DBHelper.KEY_CIG_COUNT, DBHelper.KEY_GUM_COUNT },
+													{ DBHelper.KEY_CIG_COUNT, DBHelper.KEY_NRES_CIG_COUNT,
+													  DBHelper.KEY_OTHER_COUNT, DBHelper.KEY_OTHER_TYPE_1,
+													  DBHelper.KEY_OTHER_FREE_1, DBHelper.KEY_OTHER_TYPE_2,
+													  DBHelper.KEY_OTHER_FREE_2 }
+												  };
+	List<String> columnFilter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
 		context = this;
-//		TableLayout table = new TableLayout(this);
-//		table.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		
 		setContentView(R.layout.summarize_layout);
 		table = (TableLayout) findViewById(R.id.summary_table);
 		applyID = (Button) findViewById(R.id.applyID_button);
+		
+		SharedPreferences prefs = getSharedPreferences("shiffman_calendar", 0);
+		int study = prefs.getInt("phase", 0);
+		columnFilter = new ArrayList<String>();
+		for (String key : COMMON_FILTER) {
+			columnFilter.add(key);
+		}
+		for (String key : STUDY_FILTER[study]) {
+			columnFilter.add(key);
+		}
 		
 		applyID.setOnClickListener(new OnClickListener() {
 
@@ -75,18 +94,20 @@ public class SummarizeData extends Activity {
 		columnRow.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 
 		for (String columnName : columns) {
-			TextView cell = new TextView(this);
-			cell.setText(columnName);
-			cell.setTextSize(14);
-			cell.setTextColor(Color.BLUE);
-			cell.setGravity(Gravity.CENTER);
-			cell.setPadding(2, 2, 2, 2);
-			columnRow.addView(cell);
-			
-			View vline1 = new View(this);
-			vline1.setLayoutParams(new TableRow.LayoutParams(2, TableRow.LayoutParams.MATCH_PARENT));
-			vline1.setBackgroundColor(Color.BLUE);
-			columnRow.addView(vline1);
+			if (columnFilter.contains(columnName)) {
+				TextView cell = new TextView(this);
+				cell.setText(columnName);
+				cell.setTextSize(14);
+				cell.setTextColor(Color.BLUE);
+				cell.setGravity(Gravity.CENTER);
+				cell.setPadding(2, 2, 2, 2);
+				columnRow.addView(cell);
+				
+				View vline1 = new View(this);
+				vline1.setLayoutParams(new TableRow.LayoutParams(2, TableRow.LayoutParams.MATCH_PARENT));
+				vline1.setBackgroundColor(Color.BLUE);
+				columnRow.addView(vline1);
+			}
 		}
 		table.addView(columnRow);
 		final View vline = new View(this);
@@ -100,26 +121,28 @@ public class SummarizeData extends Activity {
     		dataRow.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
     		dataRow.setShowDividers(TableRow.SHOW_DIVIDER_MIDDLE);
     		for (String columnName : columns) {
-    			TextView cell = new TextView(this);
-    			if (columnName.equalsIgnoreCase("date")) {
-    				long date = value.getAsLong(columnName);
-    				Calendar cal = Calendar.getInstance();
-    				cal.setTimeInMillis(date);
-    				SimpleDateFormat format1 = new SimpleDateFormat("d MMM yyyy", Locale.US);
-    				cell.setText(format1.format(cal.getTime()));
-    			} else {
-    				cell.setText(value.getAsString(columnName));
+    			if (columnFilter.contains(columnName)) {
+	    			TextView cell = new TextView(this);
+	    			if (columnName.equalsIgnoreCase("date")) {
+	    				long date = value.getAsLong(columnName);
+	    				Calendar cal = Calendar.getInstance();
+	    				cal.setTimeInMillis(date);
+	    				SimpleDateFormat format1 = new SimpleDateFormat("d MMM yyyy", Locale.US);
+	    				cell.setText(format1.format(cal.getTime()));
+	    			} else {
+	    				cell.setText(value.getAsString(columnName));
+	    			}
+	    			cell.setTextSize(14);
+	    			cell.setTextColor(Color.BLACK);
+	    			cell.setGravity(Gravity.CENTER);
+	    			cell.setPadding(5, 5, 5, 5);
+	    			dataRow.addView(cell);
+	    			
+	    			View vline1 = new View(this);
+	    			vline1.setLayoutParams(new TableRow.LayoutParams(2, TableRow.LayoutParams.MATCH_PARENT));
+	    			vline1.setBackgroundColor(Color.BLACK);
+	    			dataRow.addView(vline1);
     			}
-    			cell.setTextSize(14);
-    			cell.setTextColor(Color.BLACK);
-    			cell.setGravity(Gravity.CENTER);
-    			cell.setPadding(5, 5, 5, 5);
-    			dataRow.addView(cell);
-    			
-    			View vline1 = new View(this);
-    			vline1.setLayoutParams(new TableRow.LayoutParams(2, TableRow.LayoutParams.MATCH_PARENT));
-    			vline1.setBackgroundColor(Color.BLACK);
-    			dataRow.addView(vline1);
     		}
     		table.addView(dataRow);
     		final View vline1 = new View(this);
